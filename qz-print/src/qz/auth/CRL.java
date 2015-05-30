a948fec773aca17ec70ad20e74fbfda9b8755137
Tres Finocchiaro <tres.finocchiaro@gmail.com>
2015-05-30 19:59:30 -0400

    More verbose CRL

--- qz-print/src/qz/auth/CRL.java
+++ qz-print/src/qz/auth/CRL.java
@@ -1,9 +1,13 @@
 package qz.auth;
 
+import qz.common.TrayManager;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Wrapper class for the Certificate Revocation List
@@ -12,11 +16,12 @@ import java.util.ArrayList;
 public class CRL {
 
     private static CRL instance = null;
+    private static final Logger log = Logger.getLogger(TrayManager.class.getName());
 
     /**
      * The URL to the QZ CRL. Should not be changed except for dev tests
      */
-    public static final String CRL_URL = "https://crl.qz.io/";
+    public static final String CRL_URL = "https://crl.qz.io";
 
     ArrayList<String> revokedHashes = new ArrayList<String>();
     boolean loaded = false;
@@ -31,6 +36,8 @@ public class CRL {
                 @Override
                 public void run() {
                     BufferedReader br = null;
+
+                    log.log(Level.INFO, "Loading CRL " + CRL_URL + "...");
 
                     try {
                         URL qzCRL = new URL(CRL_URL);
@@ -48,9 +55,10 @@ public class CRL {
                         }
 
                         instance.loaded = true;
+                        log.log(Level.INFO, "Successfully loaded " + instance.revokedHashes.size() + " CRL entries from " + CRL_URL);
                     }
                     catch(Exception e) {
-                        e.printStackTrace();
+                        log.log(Level.WARNING, "Error loading CRL " + CRL_URL, e);
                     }
                     finally {
                         if (br != null) {
